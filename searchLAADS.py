6d728fb56b0ea2dbc59736a69ef6e36243993721
Ro <Ro@nazuna-2.local>
2017-04-29 14:39:16 +0200

    - cleaned up searchLAADS class

--- searchLAADS.py
+++ searchLAADS.py
@@ -1,64 +1,18 @@
-# import zeep
-# from zeep.plugins import HistoryPlugin
-
-# history = HistoryPlugin()
-# # client = zeep.Client(wsdl=wsdl)
-# client = zeep.Client(wsdl=wsdl, plugins = [history])
-
-# # serv = client.create_service("{http://laads.modapsws.gsfc.nasa.gov}MODAPSservicesSoap12Binding","http://modwebsrv.modaps.eosdis.nasa.gov/axis2/services/MODAPSservices")
-# # serv = client.create_service("{http://laads.modapsws.gsfc.nasa.gov}MODAPSservicesHttpBinding","http://modwebsrv.modaps.eosdis.nasa.gov/axis2/services/MODAPSservices")
-
-# # serv = client.bind("MODAPSservices", "MODAPSservicesHttpSoap12Endpoint")
-
-# # order_type = client.get_element("ns0:listProductsByInstrument")
-# # order = zeep.xsd.AnyObject(order_type, order_type("AM1M"))
-# value = zeep.xsd.AnyObject(zeep.xsd.String(), "AM1M")
-# # response = client.service.listProductsByInstrument(instrument=value)
-# client.service.listProductsByInstrument(value)
-# # print(response)
-# # response = serv.listProducts
-
-
-#============================
-#soapy
-
+import os
+import math
 from SOAPpy import WSDL
 from SOAPpy import SOAPProxy
-import math
 from datetime import datetime, timedelta
 
-# server = WSDL.Proxy(wsdl)
-# server.config.dumpSOAPOut = 1
-# server.config.dumpSOAPIn = 1
 
-# print(server.listProducts)
-
-#start and end times as datetime obj
-endtime = datetime(2017,4,27,00,00)
-starttime = datetime(2010,4,25,00,00)
-
-# endtime = "2017-04-27 00:00:00"
-# # starttime = "2015-01-01 00:00:00"
-# starttime = "2017-04-26 12:00:00"
-
-product = "MOD35_L2"
-collection = 6
-north = 2.0
-south = -15.0
-east = -66.0
-west = -82.0
-coordsOrTiles = "coords"
-dayNightBoth = "dbn"
-
-bbox = [north,south,west,east]
-
-class LAADSsearch(object):
+class searchLAADS(object):
 
     wsdl = "http://modwebsrv.modaps.eosdis.nasa.gov/axis2/services/MODAPSservices?wsdl"
     serviceNamespace = "http://modapsws.gsfc.nasa.gov"
     serviceURL = "https://modwebsrv.modaps.eosdis.nasa.gov/axis2/services/MODAPSservices"
+    server = SOAPProxy(serviceURL)
 
-    LAADSmaxFiles = 5000
+    LAADSmaxFiles = 3000
 
     def __init__(self, product, collection, stime, etime, bbox, coordsOrTiles, dayNightBoth):
         self.product = product
@@ -68,16 +22,11 @@ class LAADSsearch(object):
         self.bbox = bbox
         self.cot = coordsOrTiles
         self.dnb = dayNightBoth
+        self.fileURLs = []
 
 
-    def numTilesForBbox(bbox):
+    def numTilesForBbox(self):
         """Calculates the approximate number of tiles which cover the given extent
-
-        Parameters
-        ----------
-        bbox: list
-            Bounding box of extent given as [north, south, west, east]
-
 
         Returns
         -------
@@ -85,26 +34,16 @@ class LAADSsearch(object):
             Approximate number of tiles to be expected for given bbox.
         """
         
-        numlat = (bbox[2] - bbox[3]) / 20    #one tile is approx. 25 degrees in lat
-        numlon = (bbox[0] - bbox[1]) / 20     #and approx. 22 degrees in lon direction
+        numlat = (self.bbox[2] - self.bbox[3]) / 20    #one tile is approx. 25 degrees in lat
+        numlon = (self.bbox[0] - self.bbox[1]) / 20     #and approx. 22 degrees in lon direction
 
         # print(numlat*20, numlon*20)
         return math.ceil(abs(numlat))*2 * math.ceil(abs(numlon))*2
 
 
-    def estimFiles(numTiles, stime, etime):
+    def estimNumFiles(self):
         """Estimates the number of files that need to be downloaded
         to cover the given extent for the specified time intervall.
-
-        Parameters
-        ----------
-        numTiles: int
-            extimated tiles for bounding box calculated by numTilesForBbox
-        stime: datetime
-            starttime of time intervall
-        etime: datetime
-            endtime of time intervall
-
         
         Returns
         -------
@@ -123,27 +62,17 @@ class LAADSsearch(object):
         #for MOD35_L2 and MYD35_L2 there are actually only half of the daytime 12 hours scenes
         #available if dayNightBoth is specified
 
-        deltaHours = (etime - stime).total_seconds() / 3600
+        deltaHours = (self.etime - self.stime).total_seconds() / 3600
         numDaysNights = deltaHours / 12
-        numFiles = numDaysNights * numTiles
+        numFiles = numDaysNights * self.numTilesForBbox()
 
         return numFiles
 
 
-    def timeChunks(stime, etime, numFiles):
+    def timeChunks(self):
         """Splits the time intervall between starttime and endtime into chunks.
         The number of files returned by the "searchFiles" function is below the
         6000 limit for each time chunk.
-
-        Parameters
-        ----------
-        stime: datetime
-            starttime
-        etime: datetime
-            endtime
-        numFiles: int
-            number of Files. Result of the "estimFiles" function.
-
 
         Returns
         -------
@@ -158,23 +87,23 @@ class LAADSsearch(object):
         """
 
         #calculate number of chunks necessary
-        numChunks = numFiles / LAADSmaxFiles
+        numChunks = self.estimNumFiles() / self.LAADSmaxFiles
 
         #calculate time window for each chunk in hours
-        deltaHours = (etime - stime).total_seconds() / 3600
+        deltaHours = (self.etime - self.stime).total_seconds() / 3600
         chunkHours = deltaHours / numChunks
         #calculate chunk size to full weeks
         chunkWeeks = math.ceil(chunkHours / (7 * 24))
 
         #create list with tuples of start and end times of chunks
-        chunkStime = stime
+        chunkStime = self.stime
         chunkList = []
-        while chunkStime < etime:
+        while chunkStime < self.etime:
             chunkEtime = chunkStime + timedelta(weeks = chunkWeeks)
-            if chunkEtime < etime:
+            if chunkEtime < self.etime:
                 chunkList.append((chunkStime, chunkEtime))
             else:
-                chunkList.append((chunkStime, etime))
+                chunkList.append((chunkStime, self.etime))
 
             #set startime of next chunk to endtime of last chunk
             chunkStime = chunkEtime
@@ -190,30 +119,11 @@ class LAADSsearch(object):
         return
 
 
-    def searchFiles(prod, coll, stime, etime, bbox, coordsOrTiles, dnb):
+    def searchFiles(self):
         """Search LAADS web for files.
         Possible values of most parameters are listed in the LAADS web SOAP API
         documenation at:
         https://ladsweb.modaps.eosdis.nasa.gov/tools-and-services/lws-classic/api.php#searchForFiles
-
-
-        Parameters
-        ----------
-        prod: str
-            product name
-        coll: int
-            collection to use
-        stime: datetime
-            start date and time of search window.
-        etime: datetime
-            end date and time of search window
-        bbox: list 
-            Bounding box in Degrees given as list [north, south, west, east]
-        coordsOrTiles: str
-            Specify if bbox is given in coords or tile numbers
-        dnb:  str
-            What data to include; day, night or have both. Values: d,n,dnb and combinations
-
 
         Return
         ------
@@ -226,41 +136,59 @@ class LAADSsearch(object):
         getFileURLs: Get file URLs for file IDs
 
         """
-        server = SOAPProxy(self.serviceURL)
 
         north, south, west, east = self.bbox
+        fileURLs = []
 
-        #check if expected number of files is above limit of SOAP service.
-        #if true then get file ids iteratively with time chunks
-        numExpFiles = numFiles(stime, etime, numTilesForBbox(bbox))
-
-        if numExpFiles > LAADSmaxFiles:
-
-        starttime = stime.strftime("%Y-%m-%d %H:%M")
-        endtime = etime.strftime("%Y-%m-%d %H:%M")
-
-        fileIDs = server.searchForFiles(products=prod, collection=coll, startTime=starttime, endTime=endtime,
-                north=north, south=south, east=east, west=west, coordsOrTiles=coordsOrTiles, dayNightBoth=dnb)
-
-        return fileIDs
-
-# print(estimFiles(numTilesForBbox(bbox), starttime, endtime))
-# print(timeChunks(starttime, endtime, estimFiles(numTilesForBbox(bbox), starttime, endtime)))
-# print(len(searchFiles(product,collection,starttime,endtime,[north,south,west,east],coordsOrTiles,dayNightBoth)))
+        tchunks = self.timeChunks()
+        if len(tchunks) > 1:
+            print("It seems your are trying to search for many files. This may take a while...")
+        else:
+            print("Searching for files...")
 
 
+        for chunk in tchunks:
+            starttime = chunk[0].strftime("%Y-%m-%d %H:%M")
+            endtime = chunk[1].strftime("%Y-%m-%d %H:%M")
+
+            IDs = self.server.searchForFiles(products=self.product, collection=self.collection, startTime=starttime, endTime=endtime,
+                    north=north, south=south, east=east, west=west, coordsOrTiles=self.cot,
+                    dayNightBoth=self.dnb)
+
+            IDsFilestring = ",".join(IDs)
+            URLs = self.server.getFileUrls(fileIds=IDsFilestring)
+
+            self.fileURLs += URLs
+
+        pass
 
 
-# filestring = ",".join(fileIDs)
+    def dumpURLs(self, fname, replace = False):
+        """Write URLs to file one each row.
 
-# fileURLs = server.getFileUrls(fileIds=filestring)
-    
+        Parameters
+        ----------
+        fname: str
+            filename
+        replace: boolean (optional)
+            overwrite file if exists
 
-#============================
-#suds
+        Return
+        ------
+        """
 
-# from suds.client import Client
+        if len(self.fileURLs) < 1:
+            print("There are no URLs to write. Please retrive file URLs by using the \"searchFiles\" function first.")
+        else:
+            #check if file exists
+            if os.path.isfile(fname) & (not replace):
+                print("The file already exists. Please choose another name or set the \"replace\" parameter to True.")
+            else:
+                with open(fname, "w") as f:
+                    for i in self.fileURLs:
+                        f.write(i + "\n")
+        
+        pass
 
-# client = Client(url =  wsdl, location = serviceURL)
 
-# print(client.service.listProductsByInstrument(instrument="AM1M"))
+# if __name__ == "__main__":
