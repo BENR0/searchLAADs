d8bccfb0a883722d09f5f56c49161fed4fdfe5b2
ro <mail2benr@gmail.com>
2018-09-14 12:09:57 +0200

    Add tests for estimateHDDspace based on pytest

--- tests/test_searchLAADS.py
+++ tests/test_searchLAADS.py
@@ -1,13 +1,9 @@
-from nose.tools import *
+import pytest
 from searchlaads.searchLAADS import searchLAADS
 
-def setup():
-    print "SETUP!"
 
-def teardown():
-    print "TEAR DOWN!"
-
-def test_basic():
+@pytest.fixture(scope = "module")
+def new_domain():
 
     # server = WSDL.Proxy(wsdl)
     # server.config.dumpSOAPOut = 1
@@ -15,24 +11,27 @@ def test_basic():
 
     #start and end times as datetime obj
     endtime = "201704270000"
-    starttime = "201004250000"
-
+    starttime = "201704170000"
 
     product = "MOD35_L2"
     collection = 6
     north = 2.0
-    south = -15.0
-    east = -66.0
-    west = -82.0
+    south = -2.0
+    east = -70.0
+    west = -74.0
     coordsOrTiles = "coords"
     dayNightBoth = "dbn"
 
     bbox = [north,south,west,east]
 
     mod35 = searchLAADS(product, collection, starttime, endtime, bbox, coordsOrTiles, dayNightBoth)
+    mod35.searchFiles()
+
+    return mod35
 
 
-    mod35.dumpURLs("urls.txt", replace = True)
+def test_estimateHDDspace(new_domain):
+    size = new_domain.estimateHDDspace(nsamples= 10)
+    assert type(size) == float
 
-    print "I RAN!"
 
